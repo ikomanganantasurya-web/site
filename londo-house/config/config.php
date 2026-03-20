9c4dcc7b9398b184752a63af9eb197190e5765a0
ikomanganantasurya-web <ikomanganantasurya@gmail.com>
2026-03-19 21:10:03 -0700

    Delete londo-house directory

--- londo-house/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Configuration for LONDO HOUSE
-define('SITE_NAME', 'LONDO HOUSE');
-define('SITE_URL', '/'); // Adjust based on final deployment path
-
-// Assets shortcut
-define('ASSETS_PATH', SITE_URL . 'assets/');
-?>
--- londo-house/pages/404.php
+++
@@ -1,7 +0,0 @@
-<section class="page-header" style="padding: 15rem 0; min-height: 60vh;">
-    <div class="container text-center">
-        <h1 style="font-size: 4rem;">404</h1>
-        <p>The page you are looking for does not exist.</p>
-        <a href="index.php" class="btn btn-primary mt-2">Return Home</a>
-    </div>
-</section>
